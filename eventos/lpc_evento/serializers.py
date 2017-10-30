577e8e4b68e7ad9e1f89136cd72ec6b8a20f75ba
Vinicius <vinicavi@hotmail.com>
2017-10-30 20:53:36 -0300

    dia 30-10

--- eventos/lpc_evento/serializers.py
+++ eventos/lpc_evento/serializers.py
@@ -13,16 +13,31 @@ class PessoaSerializer(serializers.HyperlinkedModelSerializer):
         model = Pessoa
         fields = ('nome', 'email', 'sexo', 'idade', 'usuario')
 
+    def create(self, dados):
+        dados_usuario = dados.pop('usuario')
+        U = User.objects.create(**dados_usuario)
+        P = Pessoa.objects.create(usuario = U, **dados)
+        return P
+
 class EventoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Evento
         fields = ('nome', 'dataEHoraDeInicio','endereco','logotipo','sigla','eventoPrincipal')
+
+    def create(self, dados):
+        return Evento.objects.create(**dados)
 
 class TicketSerializer(serializers.HyperlinkedModelSerializer):
     idEvento = EventoSerializer(many = False)
     class Meta:
         model = Ticket
         fields = ('nome', 'descricao','valor','idEvento')
+
+    def create(self, dados):
+        dados_evento = dados.pop('idEvento')
+        E = Evento.objects.create(**dados_evento)
+        T = Ticket.objects.create(idEvento = E, **dados)
+        return T
 
 class InscricaoSerializer(serializers.HyperlinkedModelSerializer):
     Evento = EventoSerializer(many = False)
@@ -31,3 +46,11 @@ class InscricaoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Inscricao
         fields = ('Evento', 'Participante','Ticket','validacao')
+
+    def create(self, dados):
+        dados_evento = dados.pop('Evento')
+        dados_participante = dados.pop('Participante')
+        E = Evento.objects.create(**dados_evento)
+        P = Pessoa.objects.create(**dados_participante)
+        I = Inscricao.objects.create(Evento = E, Participante = P, **dados)
+        return I
